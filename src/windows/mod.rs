00becec415f9bc6b8fb69547dda9f3db4b2b1091
Ade Yahya Prasetyo <adeyahyaprasetyo@gmail.com>
2020-08-30 07:31:26 +0700

    Update mod.rs

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -155,7 +155,7 @@ unsafe extern "system" fn mouse_proc(code: c_int, w_param: WPARAM, l_param: LPAR
                 Bind::BlockableBind(cb) => {
                     let cb = Arc::clone(cb);
                     spawn(move || cb());
-                    1
+                    ()
                     // if let BlockInput::Block = cb() {
                     //    return 1;
                     // }
